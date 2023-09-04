b664a8ba2c8384f2bb361fcbd349a1b5e816dd57
Aldo <aldoandres045@gmail.com>
2023-09-04 00:56:59 -0300

    Agrego librería ChakraUI

--- homebanking/src/index.jsx
+++ homebanking/src/index.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import HomePage from './Pages/HomePage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HomePage from "./Pages/HomePage";
+import { ChakraProvider } from '@chakra-ui/react'
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HomePage />
+    <ChakraProvider>
+      <HomePage />
+    </ChakraProvider>
   </React.StrictMode>
 );
