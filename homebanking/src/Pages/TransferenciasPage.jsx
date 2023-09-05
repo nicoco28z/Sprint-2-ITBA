8ada644a2248cf391397842d4dbbd9170ce9f481
Aldo <aldoandres045@gmail.com>
2023-09-05 14:43:42 -0300

    Agrego retoques

--- homebanking/src/Pages/TransferenciasPage.jsx
+++ homebanking/src/Pages/TransferenciasPage.jsx
@@ -1,5 +1,6 @@
-import { Box, Button, Card, CardBody, HStack, Heading } from "@chakra-ui/react";
+import { Box, Button, HStack, Heading, SimpleGrid } from "@chakra-ui/react";
 import React from "react";
+import TransferenciaCard from "../components/TransferenciaCard/transferenciaCard";
 
 export default function TransferenciasPage() {
   return (
@@ -9,22 +10,27 @@ export default function TransferenciasPage() {
       <Heading textAlign="center" mt="10px">
         Transferencias
       </Heading>
-      <Box display="flex" justifyContent="center" textAlign="center" mt="2rem">
-        <Card size="md" w="50%">
-          <CardBody>
-            <HStack justifyContent="center">
-              <Button>Transferir con Alias, CBU o CVU</Button>
-              <Button>Ingresar Dinero</Button>
-            </HStack>
-          </CardBody>
-        </Card>
+      <Box display="grid" textAlign="center" mt="2rem" p="20px">
+        <HStack justifyContent="center">
+          <Button>Transferir con Alias, CBU o CVU</Button>
+          <Button>Ingresar Dinero</Button>
+        </HStack>
       </Box>
-      <Box p='35px' borderRadius='25px' border='1px' w='60rem' borderColor='black'>
-        <Heading size='md' textAlign="start">Transferir a contactos</Heading>
-        <Button leftIcon>Agregar Contacto + </Button>
-       
+      <Box ml="5rem">
+        <Heading size="md" textAlign="start">
+          Transferir a contactos
+        </Heading>
+        <Button mt="5px">Agregar Contacto + </Button>
+        <SimpleGrid
+          spacing={4}
+          templateColumns="repeat(auto-fill, minmax(120px, 1fr))"
+          mt='10px'
+        >
+          <TransferenciaCard name="Aldo Andres" />
+          <TransferenciaCard name="Nicolas Colombo" />
+          <TransferenciaCard name="Harold" />
+        </SimpleGrid>
       </Box>
-
     </>
   );
 }
