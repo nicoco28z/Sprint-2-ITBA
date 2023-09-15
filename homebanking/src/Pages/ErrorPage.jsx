a7d04f950ee1c18b4d7d5a64c6337151d1d1b9bf
Aldo <aldoandres045@gmail.com>
2023-09-15 18:42:38 -0300

    Agregada imagen de error 404

--- homebanking/src/Pages/ErrorPage.jsx
+++ homebanking/src/Pages/ErrorPage.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
-import {useAuth} from '../hooks/useAuth'
+import { useNavigate } from "react-router-dom";
+import { Button, HStack, Image, Text } from "@chakra-ui/react";
+
+
 
 export default function ErrorPage() {
 
-  const auth = useAuth()
+  const navigate = useNavigate();
 
   return (
     <>
-      {auth.isLogged? "Hola" : "Adios"}
+      <Text textAlign="center" fontSize="xl">
+        Página no encontrada.
+      </Text>
+      <Image
+        src="./error_page_img.jpg"
+        objectFit="cover"
+        objectPosition="50% 50%"
+        boxSize="60vh"
+        alt="Error 404, página no encontrada"
+        margin="auto"
+      />
+      <HStack justify="center">
+        <Button onClick={() => navigate(-1)}>Retroceder</Button>
+        <Button onClick={() => navigate("/home")}>Volver al inicio</Button>
+      </HStack>
     </>
-  )
+  );
 }
